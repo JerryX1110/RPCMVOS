47fb80aff5ad24522bb70dd00d7debb718296d01
Xiaohao Xu <65257938+JerryX1110@users.noreply.github.com>
2021-12-14 15:37:28 +0800

    Delete networks directory

--- networks/deeplab/backbone/__init__.py
+++
@@ -1,9 +0,0 @@
-from networks.deeplab.backbone import resnet, mobilenet
-
-def build_backbone(backbone, output_stride, BatchNorm):
-    if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm)
-    elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(output_stride, BatchNorm)
-    else:
-        raise NotImplementedError
